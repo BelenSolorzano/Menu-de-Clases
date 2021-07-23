ed8f767b8f7c05461abdd67d7b5143a901d1fb2c
BelenSolorzano <mbsolorzano98@gmail.com>
2021-07-23 01:01:14 -0500

    Menu de clases

---
+++ Lista.py
@@ -0,0 +1,34 @@
+
+class Lista:
+    def __init__(self, lista):
+        self.lista1 = lista
+
+    def  presentarLista():
+        pass
+    def  buscarLista(valor):
+        pass
+    def  listaFactorial(numero): 
+        listaFactorial= " "
+        if (numero == 0):
+            return 1
+        else: 
+            return numero * listaFactorial(numero - 1)
+    numero = int(input("Ingrese un número: ")) 
+    print("El factorial de : {}" + str(numero) + ": es: {}"+ str(listaFactorial(numero)))
+
+       
+    def listaPrimo():
+        pass
+    def listaNotas(listaNotasDicccionario): 
+        pass
+    def insertarLista(valor,posicion): 
+        pass
+    def eliminarLista(valor): 
+        pass
+    def retornaValorLista(posicion): 
+        pass
+    def copiarTuplaLista(tupla):
+        pass
+    def vueltoLista(listaClientesDiccionario):
+        pass
+li = Lista()
